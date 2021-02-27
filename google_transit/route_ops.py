094e0cb7af30948e9e0495a5af9f552b07b0d0bd
Jacob Chambers <wchambers8@gatech.edu>
2021-02-26 19:22:57 -0500

    Change routes storage to dictionary for better performance, add main code execution

--- google_transit/route_ops.py
+++ google_transit/route_ops.py
@@ -1,15 +1,16 @@
 import csv
+import json
 
 def load_routes_csv():
-    routes = []
+    routes = {}
     with open("routes.txt", "r") as route_file:
         reader = csv.reader(route_file)
         next(reader)
 
         for row in reader:
             route = {"id": row[0], "number": row[1], "name": row[2],
-                    "type": row[4], trips: []}
-            routes.append(route)
+                    "type": row[4], "trips": []}
+            routes[row[0]] = route
 
     return routes
 
@@ -20,5 +21,22 @@ def load_trips_csv():
         next(reader)
 
         for row in reader:
-            trip = {"route_id": row[0], "id": row[2], 
+            trip = {"route_id": row[0], "id": row[2],
                     "headsign": row[3], "direction": row[4]}
+            trips.append(trip)
+
+    return trips
+
+
+
+if __name__ == "__main__":
+    routes = load_routes_csv()
+    trips = load_trips_csv()
+
+    for trip in trips:
+        # Find the route entry for this trip and add the trip id to the route entry
+        matching_route = routes[trip["route_id"]]
+        matching_route["trips"].append(trip["id"])
+
+    with open("bus_routes.json", "w") as routes_file:
+        json.dump(routes, routes_file)
